53d6f279fefc5e27abd9c03e36297e02a00f0e58
Phillip Merensky <mail@phillme.de>
2016-02-05 22:46:05 +0100

    Checking Todos and minor cleanup

--- src/main/java/de/phillme/PhotoExtension.java
+++ src/main/java/de/phillme/PhotoExtension.java
@@ -1,11 +1,69 @@
+/*
+ *     This file is part of photosorter.
+ *
+ *     photosorter is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     photosorter is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *     Diese Datei ist Teil von photosorter.
+ *
+ *     photosorter ist Freie Software: Sie können es unter den Bedingungen
+ *     der GNU General Public License, wie von der Free Software Foundation,
+ *     Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ *     veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ *     photosorter wird in der Hoffnung, dass es nützlich sein wird, aber
+ *     OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ *     Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ *     Siehe die GNU General Public License für weitere Details.
+ *
+ *     Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ *     Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ *     This file is part of photosorter.
+ *
+ *     photosorter is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     photosorter is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *     Diese Datei ist Teil von photosorter.
+ *
+ *     photosorter ist Freie Software: Sie können es unter den Bedingungen
+ *     der GNU General Public License, wie von der Free Software Foundation,
+ *     Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ *     veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ *     photosorter wird in der Hoffnung, dass es nützlich sein wird, aber
+ *     OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ *     Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ *     Siehe die GNU General Public License für weitere Details.
+ *
+ *     Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ *     Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ */
+
 package de.phillme;
 
-/**
- * Copyright: Lirdy UG(haftungsbeschränkt)
- * Author: Phillip Merensky
- * Date: 05.01.16
- * Time: 19:01
- */
 public class PhotoExtension {
 
     public String extensionWithoutDot;
